05243ef3adb329f7ab028c48be32d917ad2883ee
Felipe Eduardo <felipeeduardodealmeida@gmail.com>
2023-09-15 12:49:40 -0300

    resolução de desafio

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,9 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        protected string Modelo { get; set; }
-        protected string IMEI { get; set; }
-        protected int Memoria { get; set; }
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
         public Smartphone(string numero, string modelo, string imei, int memoria)
         {
